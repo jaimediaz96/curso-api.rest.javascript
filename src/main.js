48348f5390fde6f5fa404bd54a42069ee16bf59c
JaimeDiaz <jedt1996@gmail.com>
2023-09-19 12:04:39 -0500

    Migracion a axios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
+const api = axios.create({
+    baseURL: "https://api.themoviedb.org/3/",
+    headers: {
+        "Content-Type": "application/json;charset=utf-8"
+    },
+    params: {
+        "api_key": API_KEY
+    }
+});
+
 async function getTrendingMoviesPreview() {
-    const res = await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY);
-    const data = await res.json();
+    const { data } = await api("trending/movie/day");
 
     const movies = data.results;
     movies.forEach(movie => {
@@ -20,8 +29,7 @@ async function getTrendingMoviesPreview() {
 }
 
 async function getGenresPreview() {
-    const res = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_KEY);
-    const data = await res.json();
+    const { data } = await api("genre/movie/list");
 
     const genres = data.genres;
     genres.forEach(genre => {
