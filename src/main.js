6d298287f63e0f77db03ef6ee19c5d44d4def61f
JaimeDiaz <jedt1996@gmail.com>
2023-09-20 17:14:51 -0500

    Mostrando y ocultando secciones

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ async function getTrendingMoviesPreview() {
 
     const movies = data.results;
     movies.forEach(movie => {
-        const trendingPreviewMoviesContainer = document.querySelector("#trendingPreview .trendingPreview-movieList");
+        const trendingMoviesPreviewList = document.querySelector("#trendingPreview .trendingPreview-movieList");
 
         const movieContainer = document.createElement("div");
         movieContainer.classList.add("movie-container");
@@ -24,7 +24,7 @@ async function getTrendingMoviesPreview() {
         movieImg.setAttribute("src", "https://image.tmdb.org/t/p/w300/" + movie.poster_path);
 
         movieContainer.appendChild(movieImg);
-        trendingPreviewMoviesContainer.appendChild(movieContainer);
+        trendingMoviesPreviewList.appendChild(movieContainer);
     });
 }
 
@@ -33,7 +33,7 @@ async function getGenresPreview() {
 
     const genres = data.genres;
     genres.forEach(genre => {
-        const previewGenresContainer = document.querySelector("#categoriesPreview .categoriesPreview-list");
+        const categoriesPreviewList = document.querySelector("#categoriesPreview .categoriesPreview-list");
 
         const genresContainer = document.createElement("div");
         genresContainer.classList.add("category-container");
@@ -46,6 +46,6 @@ async function getGenresPreview() {
         genresTitle.appendChild(genresTitleText);
 
         genresContainer.appendChild(genresTitle);
-        previewGenresContainer.appendChild(genresContainer);
+        categoriesPreviewList.appendChild(genresContainer);
     });
 }
